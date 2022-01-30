2339ab4cd824fa8916bdc840597cbe6caf40b4d4
claudiocassimiro <days_surfing@live.com>
2022-01-30 14:00:56 -0300

    Fix export

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export default Main = styled.main`
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: column;
 `;
+
+export default Main;
